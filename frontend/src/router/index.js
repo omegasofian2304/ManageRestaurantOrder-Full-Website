02d671bcb27c0336664fffc7fcfbc5b8d7fc2eed
Sofian Hussein <sofian.hussein@eduvaud.ch>
2026-05-22 09:09:07 +0200

    Feature/us 11 list orders (#35)
    * feat: add the beginning of the orders page
    
    * feat: add order page
    
    * fix: change the tailwind configuration

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/login.vue';
+import Login from '../views/Login.vue';
 import {useAuthStore } from "@/stores/authStore.js";
 import MealsView from "@/views/MealsView.vue";
 import CreateEmployeeView from "@/views/CreateEmployeeView.vue";
+import OrdersView from "@/views/OrdersView.vue";
 
 const routes = [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login, name: 'login' },
     { path: '/meals', component: MealsView, name: 'meals', meta: { requiresAuth: true } },
+    { path: '/orders', component: OrdersView, name: 'orders', meta: { requiresAuth: true } },
     { path: '/employees/create', component: CreateEmployeeView, name: 'createEmployee', meta: { requiresAuth: true, requiresAdmin: true } }
 ];
 
@@ -30,13 +32,13 @@ router.beforeEach(async (to) => {
         return '/login'
     }
 
-    // admin verification
+    // manager verification
     if (to.meta.requiresManager && authStore.post !== 'admin' && authStore.post !== 'manager') {
         return '/login'
     }
 
     if (to.path === '/login' && !authStore.isTokenExpired) {
-        return '/home'
+        return '/meals'
     }
 })
 
